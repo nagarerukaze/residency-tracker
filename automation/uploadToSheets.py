8b3ca1bffaf8593d538c5ff838f565c86d947916
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2024-10-05 11:07:12 +0800

    changed file names and directory

--- automation/uploadToSheets.py
+++ automation/uploadToSheets.py
@@ -1,23 +1,25 @@
+from automation.googleSheetsHandler import googleSheetsHandler
 import sqlite3
 
-def main():
+googleSheets = googleSheetsHandler("1heMguJXNc2EZncSO7rhVwlQz78iqF54K7-XiYL62I-4", "Logs!A2:D")
+
+connection = sqlite3.connect("rehearsal-hall.db")
+cursor = connection.cursor()
+
+currentTime = None # get time format from aero
+
+cursor.execute("""SELECT * 
+                    FROM time_logs WHERE DATE(time_in) is DATE('now', 'localtime')""")
+
+
+rows = cursor.fetchall()
+
+print(rows)
+
+if rows == None:
+    pass
+else:
+    print(rows)
     
-    connection = sqlite3.connect("rehearsal-hall.db")
-    cursor = connection.cursor()
-    
-    currentTime = None # get time format from aero
-    
-    cursor.execute("""SELECT * 
-                      FROM time_logs 
-                      WHERE julianday(time_in) is julianday(?)""", (currentTime,))
-    
-    rows = cursor.fetchmany()
-    
-    if rows == None:
-        pass
-    else:
-        pass # call google sheets api and upload to google sheets
-    
-    
-if __name__ == "__main__":
-    main()
+    googleSheetsHandler.insertRows(googleSheets, rows)
+    pass # call google sheets api and upload to google sheets
