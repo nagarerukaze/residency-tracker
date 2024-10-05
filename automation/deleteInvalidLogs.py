8affc114c415c9cd4cac5868dfa9eaf06877ce50
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2024-10-05 11:17:40 +0800

    Update deleteInvalidLogs.py

--- automation/deleteInvalidLogs.py
+++ automation/deleteInvalidLogs.py
@@ -5,8 +5,8 @@ connection = sqlite3.connect("rehearsal-hall.db")
 cursor = connection.cursor()
 
 # Execute DELETE query where the date of `time_in` is the current date
-cursor.execute("""DELETE FROM time_logs 
-                  WHERE duration_minutes = NULL""")
+cursor.execute("""DELETE * FROM time_logs 
+                  WHERE duration_minutes is NULL""")
 
 # Commit the changes to the database
 connection.commit()
