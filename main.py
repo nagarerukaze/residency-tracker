5838eda0d3791b703dce11fe1371690494ff710a
aaronjardenil <aaron_randall_jardenil@dlsu.edu.ph>
2024-10-05 09:57:36 +0800

    added sql query

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 from automation.googleSheetsHandler import googleSheetsHandler
+import sqlite3
 
 googleSheets = googleSheetsHandler("1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms", "Class Data!A2:E")
 
+connection = sqlite3.connect("rehearsal-hall.db")
+cursor = connection.cursor()
+
+currentTime = None # get time format from aero
+
+cursor.execute("""SELECT * 
+                    FROM time_logs 
+                    WHERE julianday(time_in) is julianday(?)""", (currentTime,))
+
+rows = cursor.fetchmany()
+
+if rows == None:
+    pass
+else:
+    print(rows)
+    pass # call google sheets api and upload to google sheets
